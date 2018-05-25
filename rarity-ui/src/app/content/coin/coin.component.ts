d3a83116b9ae72d2047f1db034cecf6b8ba59b2b
Oleksandr Fedoras <oleksandr.fedoras@bonial.com>
2018-05-25 17:06:41 +0300

    Fixed UI layout issues

--- rarity-ui/src/app/content/coin/coin.component.ts
+++ rarity-ui/src/app/content/coin/coin.component.ts
@@ -9,21 +9,18 @@ import { Component, OnInit, Input } from '@angular/core';
   }
 })
 export class CoinComponent implements OnInit {
-
-  /*coin: any = {
-            valueId: 'dd308f3f-fe61-4398-a12b-fa1932979ea2',
-            coinId: 'e7d44e4d-8d38-4365-9e62-401610910f15',
-            date: '07/05/1995',
-            obverse: "https://bank.gov.ua/control/uk/currentmoney/image?id=1&g=2&s=0",
-            reverse: "https://bank.gov.ua/control/uk/currentmoney/image?id=1&g=2&s=1",
-            metal: 'мельхіор'
-        };*/
   
   @Input() coin: any;      
-        
+  
+  private IMG_BASE_URL: string = 'https://bank.gov.ua/control/uk/currentmoney/image?';
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  buildImageUrl(image: string): string {
+    return this.IMG_BASE_URL + image;
   }
 
 }
